fe2e405beaeddd8d348cfac0674c4570b8c87337
Jrsgslb <58294498+Jrsgslb@users.noreply.github.com>
2021-03-29 00:59:06 +0800

    Delete mirai-cpp directory

--- mirai-cpp/include/mirai/mirai.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef mirai_cpp__mirai_h_H_
-#define mirai_cpp__mirai_h_H_
-
-#include "mirai/defs/defs.hpp"
-#include "mirai/events/events.hpp"
-#include "mirai/mirai_bot.hpp"
-
-#endif // !mirai_cpp__mirai_h_H_
